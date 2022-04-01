759d3c02cdff788afaf107bbb02b3c490741fb90
Olli Petäjäniemi <petajaol@gmail.com>
2022-04-01 11:37:17 +0300

    Lisätty CRUD-operaatiot asiakkaalle ja proseduureille. Lisätty SQL-dump-template.

--- api/app.js
+++ api/app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var helmet = require("helmet");
+var cors = require("cors");
+
+var asiakasRouter = require("./routes/asiakas");
+var proseduuriRouter = require("./routes/proseduuri");
+//var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(helmet());
+app.use(cors());
+
+//app.use("/users", usersRouter);
+app.use("/asiakas", asiakasRouter);
+app.use("/proseduuri", proseduuriRouter);
 
 module.exports = app;
