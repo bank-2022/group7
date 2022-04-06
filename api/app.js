0d26bfefe3c7f53b0d8c02c3d0d426d2fb786378
Magusfunk <mika.valiaho@gmail.com>
2022-04-06 12:04:35 +0300

    lisätty tarvittavat rivit routelle sekä lisätty webtokenia varten omat rivinsä sekä metodi

--- api/app.js
+++ api/app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan')
+var jwt = require('jsonwebtoken');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +15,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use(authenticateToken)
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
 
 module.exports = app;
