60051807673bf727874f47ce108d672caecab40a
Magusfunk <mika.valiaho@gmail.com>
2022-04-27 08:13:25 +0300

    Bugikorjaus kortin lukitukseen

--- bankautomat/mainwindow.cpp
+++ bankautomat/mainwindow.cpp
@@ -145,6 +145,8 @@ void MainWindow::kirjautumisHandler(events e)
     } else if (e == kirjauduUlos){
         pageHandler(tervetuloaPage, false, false, "");
         webToken.clear();
+        ajastin->stop();
+        loginAttempts = 0;
     }
 
 }
